c3df550dc10573f520e9e7365000166341c8b3bc
Gabriel Genest <9.genest.gabriel@gmail.com>
2026-03-15 16:11:52 -0400

    Update base_model_testing.py
    Mise à jour de la sauvegarde de fichiers (paths)

--- projetIntegrateur/base_model_testing.py
+++ projetIntegrateur/base_model_testing.py
@@ -44,32 +44,33 @@ if __name__ == '__main__':
     lr = 1e-4
     callback_base_name = "checkpoints_base_model_v1"
     train_and_valid_Adam(device, "metadata.csv", data_root_500, model_saves, lr, batch_size, np.inf, n_epochs, 0.8,
-                         callback_base_name, "t1.npz", cnn_out_channels=(16, 32, 64))
+                         callback_base_name, "predictions/predictions_v1.npz.", cnn_out_channels=(16, 32, 64))
 
     # v2
     n_epochs = 100
     lr = 1e-4
     callback_base_name = "checkpoints_base_model_v2"
     train_and_valid_Adam(device, "metadata.csv", data_root_500, model_saves, lr, batch_size, np.inf, n_epochs, 0.8,
-                         callback_base_name, cnn_out_channels=(8, 8), cnn_mlp_out=32, gru_hidden_size=64)
+                         callback_base_name, "predictions/predictions_v2.npz.", cnn_out_channels=(8, 8), cnn_mlp_out=32,
+                         gru_hidden_size=64)
 
     # v3
     n_epochs = 100
     lr = 1e-4
     callback_base_name = "checkpoints_base_model_v3"
     train_and_valid_Adam(device, "metadata.csv", data_root_5000, model_saves, lr, batch_size, np.inf, n_epochs, 0.8,
-                         callback_base_name, cnn_out_channels=(8, 16))
+                         callback_base_name, "predictions/predictions_v3.npz.", cnn_out_channels=(8, 16))
 
     # v4
     n_epochs = 100
     lr = 1e-3
     callback_base_name = "checkpoints_base_model_v4"
     train_and_valid_Adam(device, "metadata.csv", data_root_5000, model_saves, lr, batch_size, np.inf, n_epochs, 0.8,
-                         callback_base_name, cnn_out_channels=(8, 16))
+                         callback_base_name, "predictions/predictions_v4.npz.", cnn_out_channels=(8, 16))
 
     # v5
     n_epochs = 100
     lr = 5e-4
     callback_base_name = "checkpoints_base_model_v5"
     train_and_valid_Adam(device, "metadata.csv", data_root_5000, model_saves, lr, batch_size, np.inf, n_epochs, 0.8,
-                         callback_base_name, cnn_out_channels=(8, 16))
+                         callback_base_name, "predictions/predictions_v5.npz.", cnn_out_channels=(8, 16))
